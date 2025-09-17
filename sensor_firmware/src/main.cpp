ae445219acc58120b0fa7db3f229c998c1198a2a
jamieashton22 <jamie.ashton@student.manchester.ac.uk>
2025-09-17 11:13:15 +0100

    First firmware code to get distance measurement from sensor

---
+++ sensor_firmware/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+#define TRIG 5 // trig pin
+#define ECHO 6 // echo pin
+
+float duration, distance;
+
+float get_distance(float _duration);
+
+void setup(){
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+}
+
+void loop(){
+
+  digitalWrite(TRIG, LOW);
+  delay(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn(ECHO, HIGH);
+  float distance = get_distance(duration);
+
+
+}
+
+
+float get_distance(float _duration){
+
+  return (0.5 * 0.0343 * _duration);    // cm/us  
+
+}
+
