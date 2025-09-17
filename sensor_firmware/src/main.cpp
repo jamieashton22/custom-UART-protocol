da3d758cd48b41dffe14383ec93245d6247b0d88
jamieashton22 <jamie.ashton@student.manchester.ac.uk>
2025-09-17 15:16:34 +0100

    Constructed payload bytes, header bytes, checksum byte and escaping function

--- sensor_firmware/src/main.cpp
+++ sensor_firmware/src/main.cpp
@@ -1,19 +1,40 @@
+// Arduino Firmware
+
+// loop: assemble frame (header, payload, checksum)
+//        send start flag
+//        call escaping function on every byte in frame
+//        send end flag
+
+// need function to perform escaping ...
+//==========================================================
+
+
 #include <Arduino.h>
 
 #define TRIG 5 // trig pin
 #define ECHO 6 // echo pin
 
-float duration;
+#define FLAG 0x7E 
+#define ESC 0x7D 
 
+byte header[2];
+
+// escaping function
+
+void EscapeAndSend(byte _byte);
 
 void setup(){
 
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
+  Serial.begin(9600);
+
 }
 
 void loop(){
+
+  //payload
 
   digitalWrite(TRIG, LOW);
   delay(2);
@@ -21,9 +42,38 @@ void loop(){
   delayMicroseconds(20);
   digitalWrite(TRIG, LOW);
 
-  duration = pulseIn(ECHO, HIGH);
+  float duration = pulseIn(ECHO, HIGH); // returns integer in microseconds
+
+  // convert duration from integer to bytes 
+  byte duration_int = uint16_t(duration);
+  byte payload_high = highByte(duration_int);
+  byte payload_low = lowByte(duration_int);   // these are the payload bytes
+
+  // header
+
+  byte type = 0x01;
+  byte length = 2;
+  header[0] = type;
+  header[1] = length;
+
+  // checksum 
+  byte checksum = header[0] ^ header[1] ^ payload_high ^ payload_low;
+  
+
 
 }
 
+void EscapeAndSend(byte _byte){
 
+  if(_byte == FLAG || _byte == ESC) {
+
+    Serial.write(ESC);            // send escape byte
+    Serial.write(_byte ^ 0x20);   // send original byte XORed with 0x20
+  }
+
+  else {
+    Serial.write(_byte);
+  }
+  
+}
 
